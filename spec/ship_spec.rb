e91f6aeba050f6248750f793557b0b1dea315673
Melchor De La Rosa <melchordelarosa41@yahoo.com>
2024-07-19 16:48:42 -0400

    Added tests for name,length

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -14,6 +14,13 @@ RSpec.describe Ship do
       expect(@cruiser).to be_an_instance_of(Ship)
     end
     
+    it 'has a name' do 
+      expect(@cruiser.name).to eq ("Cruiser")
+    end
+
+    it 'has length' do 
+      expect(@cruiser.length).to eq(3)
+    end
   end
 
 
