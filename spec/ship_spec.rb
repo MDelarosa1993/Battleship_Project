27519f91b27955f8a1d53ea1a0f266838291fc0c
Joel Davalos <jsdavalos@gmail.com>
2024-07-20 01:19:59 -0600

    Submarine exists

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -7,11 +7,13 @@ end
 RSpec.describe Ship do 
   before(:each) do 
     @cruiser = Ship.new("Cruiser", 3)
+    @submarine = Ship.new("Submarine", 2)
   end
 
   describe 'Initialize' do 
     it 'exists' do 
       expect(@cruiser).to be_an_instance_of(Ship)
+      expect(@submarine).to be_an_instance_of(Ship)
     end
     
     it 'has a name' do 
