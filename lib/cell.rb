fe9c69429ae31c7972b1b22090090e0013b68c34
Melchor De La Rosa <melchordelarosa41@yahoo.com>
2024-07-22 02:39:16 -0400

    Updated the methods but left the old code commented

--- lib/cell.rb
+++ lib/cell.rb
@@ -20,27 +20,34 @@ attr_reader :coordinate, :ship, :fired_upon
   end
 
   def fire_upon
-    if !empty? && !@fired_upon
-      @ship.hit
-    end
     @fired_upon = true
+    @ship.hit if @ship
+    # if !empty? && !@fired_upon
+    #   @ship.hit
+    # end
+    # @fired_upon = true
   end
 
-  def render(reveal = false) 
-    if reveal && !empty?
-      return "S"
-    end
-    if !fired_upon?
-      return "."
-    end
-    if fired_upon && empty?
-      return "M"
-    end
-    if fired_upon? && !empty? && !@ship.sunk?
-      return "H"
-    end
-    if fired_upon? && !empty? && @ship.sunk?
-      return "X"
-    end
+  def render(reveal = false)
+    return "X" if @ship&.sunk?
+    return "H" if @fired_upon && !@ship.nil?
+    return "M" if @fired_upon && @ship.nil?
+    return "S" if reveal && !@fired_upon && !@ship.nil?
+    return "." if !@fired_upon
+    # if reveal && !empty?
+    #   return "S"
+    # end
+    # if !fired_upon?
+    #   return "."
+    # end
+    # if fired_upon && empty?
+    #   return "M"
+    # end
+    # if fired_upon? && !empty? && !@ship.sunk?
+    #   return "H"
+    # end
+    # if fired_upon? && !empty? && @ship.sunk?
+    #   return "X"
+    # end
   end
 end
